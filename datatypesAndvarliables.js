404cc16e985046149ef3e157446a83b7b1f25f4f
Arindashiphura <ashiphura@gmail.com>
2024-07-02 15:21:17 +0300

    add pages

---
+++ datatypesAndvarliables.js
@@ -0,0 +1,25 @@
+//numbers
+//strings/letters
+//booleans
+//undefined
+//null
+console.log(typeof "shiphura") //string and it has to be quoted
+console.log(typeof 8) //integer
+console.log(8.5)//a float point number
+console.log(typeof null)//undefined
+console.log(typeof false)
+let emptyValue=null
+let firstNme
+console.log(firstName)
+
+
+//varliables
+let x=2
+console.log(x)
+//dont write avarliable name with a numberuse camel case naming, no space and do not start with a chararcter
+let capitalcity= "Kampala"
+let isMarried= true
+
+const PI=3.14
+var currentlocation= "Home"
+console.log(home)
---
+++ hello.js
@@ -0,0 +1,8 @@
+//prints hellow world to the consel
+console.log("hello world")
+//this is my first comment in javascript
+/*multiple comment*/
+console.log("hello","world")
+//quates are for words and numbers dont use any
+console.log(5+6)
+
